443ff37dd2f6b67e19a17c4b7512ace80462ed90
Kirk Byers <ktbyers@twb-tech.com>
2017-03-03 16:04:21 -0800

    Class1 exercises

---
+++ class1/exercises/exercise1.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+"""
+Create a YAML list in a file.
+
+Use Python to read it and print it to the screen. The list should have at least four elements.
+"""
+from __future__ import print_function
+import yaml
+from pprint import pprint
+
+
+def read_yaml(filename):
+    with open(filename) as f:
+        return yaml.load(f)
+
+
+if __name__ == "__main__":
+
+    filename = "exercise1.yml"
+    pprint(read_yaml(filename))
---
+++ class1/exercises/exercise2.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+"""
+Create a YAML dictionary in a file. Use Python to read it and print it to the screen. The
+dictionary should have at least four keys; one of the values in the dictionary should be a list.
+"""
+from __future__ import print_function
+import yaml
+from pprint import pprint
+
+
+def read_yaml(filename):
+    with open(filename) as f:
+        return yaml.load(f)
+
+
+if __name__ == "__main__":
+
+    filename = "exercise2.yml"
+    pprint(read_yaml(filename))
